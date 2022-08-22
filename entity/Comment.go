e10542ee3c1e7e6f495d5baaef5aad9f54e64bd2
jdfcitecky <jdfcitecky@gmail.com>
2022-08-22 17:37:12 +0800

    comment entity recover

--- entity/Comment.go
+++ entity/Comment.go
@@ -8,9 +8,12 @@ type Comment struct {
 	ID         int       `json:"id"`
 	MemberID   int       `json:"member_id"`
 	MemberName string    `gorm:"column:member_name" json:"member_name"`
+	Member     Member    `gorm:"constraint:OnUpdate:CASCADE,OnDelete:SET NULL;"`
 	Text       string    `gorm:"column:txet" json:"text"`
 	IsNew      bool      `gorm:"column:is_new;defalut:true" json:"is_new"`
 	WorkID     int       `json:"work_id"`
+	WorkName   string    `gorm:"column:work_name" json:"work_name"`
+	Work       Work      `gorm:"constraint:OnUpdate:CASCADE,OnDelete:SET NULL;"`
 	CreatedAt  time.Time `json:"created_at"`
 	UpdatedAt  time.Time `json:"updated_at"`
 }
