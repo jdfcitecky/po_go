74f0f5bc3f3ecb835a05a7a497d3e5cce50ee648
jdfcitecky <jdfcitecky@gmail.com>
2022-09-02 08:20:54 +0800

    upload api fixed that frontend can read the url after uploaded

--- admin/file.go
+++ admin/file.go
@@ -1,7 +1,11 @@
 package admin
 
 import (
+	"fmt"
+	"path"
 	"po_go/utils"
+	"strconv"
+	"time"
 
 	"github.com/gin-gonic/gin"
 )
@@ -10,42 +14,29 @@ import (
 func Upload(c *gin.Context) {
 	logger := utils.Log()
 	logger.Info("File upload")
-	result := make(map[string]interface{})
-
-	file, err := c.FormFile("file") // get file from form input name 'file'
+	// get file from form input name 'file'
+	file, err := c.FormFile("file")
 	if err != nil {
 		res := &utils.Response{Code: 1000, Msg: "file upload error"}
 		res.Json(c)
 		return
 	}
+	// createpath
+	now := time.Now()
+	ext := path.Ext(file.Filename)
+	fileName := strconv.Itoa(now.Nanosecond()) + ext
+	filePath := fmt.Sprintf("%s%s%s%s",
+		"./static/upload/",
+		fmt.Sprintf("%04d", now.Year()),
+		fmt.Sprintf("%02d", now.Month()),
+		fmt.Sprintf("%04d", now.Day()))
+	utils.CreateDir(filePath)
+	fullPath := filePath + "/" + fileName
 
-	c.SaveUploadedFile(file, "./static/"+file.Filename) // save file to tmp folder in current directory
-	result["url"] = ("static/" + file.Filename)
-	res := &utils.Response{Code: 0, Msg: "", Data: result}
+	// save file to tmp folder in current directory
+	c.SaveUploadedFile(file, fullPath)
+	url := (fullPath[1:])
+	res := &utils.Response{Code: 0, Msg: "", Data: url}
 	res.Json(c)
 
-	// file, err := c.FormFile("file")
-	// if err != nil {
-	// 	res := &utils.Response{Code: 1000, Msg: "文件上传错误"}
-	// 	res.Json(c)
-	// 	return
-	// }
-
-	// now := time.Now()
-	// ext := path.Ext(file.Filename)
-	// fileName := strconv.Itoa(now.Nanosecond()) + ext
-	// filePath := fmt.Sprintf("%s%s%s%s",
-	// 	"./static/upload/",
-	// 	fmt.Sprintf("%04d", now.Year()),
-	// 	fmt.Sprintf("%02d", now.Month()),
-	// 	fmt.Sprintf("%04d", now.Day()))
-
-	// utils.CreateDir(filePath)
-
-	// fullPath := filePath + "/" + fileName
-	// c.SaveUploadedFile(file, fullPath)
-	// url := fullPath[1:len(fullPath)]
-	// res := &utils.Response{Code: 0, Msg: "ok", Data: url}
-	// res.Json(c)
-	// return
 }
