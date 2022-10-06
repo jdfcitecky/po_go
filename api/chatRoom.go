3f9bc786c3f8959b94bc06fcb7fbeba13e72a70d
jdfcitecky <jdfcitecky@gmail.com>
2022-10-06 10:23:24 +0800

    add a api to retrieve messages list limit by page

--- api/chatRoom.go
+++ api/chatRoom.go
@@ -40,6 +40,47 @@ func ChatRoomList(c *gin.Context) {
 	res.Json(c)
 }
 
+func ChatRoomMessagesListWithLimit(c *gin.Context) {
+	logger := utils.Log()
+	logger.Info("Get Messages List")
+	json := make(map[string]interface{})
+	err := c.BindJSON(&json)
+	if err != nil {
+		res := &utils.Response{Code: 1000, Msg: "Data format wrong"}
+		res.Json(c)
+		return
+	}
+	chatRoomMessages := new(service.ChatRoomMessage)
+
+	//get chat room Id
+	chatRoomID, err := strconv.Atoi(utils.StrVal(json["chat_room_id"]))
+	if err != nil {
+		res := &utils.Response{Code: 1000, Msg: "Query fail", Data: ""}
+		res.Json(c)
+	}
+	pageStart, err := strconv.Atoi(utils.StrVal(json["page_start"]))
+	if err != nil {
+		res := &utils.Response{Code: 1000, Msg: "Query fail", Data: ""}
+		res.Json(c)
+	}
+	pageLimit, err := strconv.Atoi(utils.StrVal(json["page_limit"]))
+	if err != nil {
+		res := &utils.Response{Code: 1000, Msg: "Query fail", Data: ""}
+		res.Json(c)
+	}
+
+	//query list
+	result := chatRoomMessages.FindChatRoomMessagesListByChatRoomIDWithLimit(chatRoomID, pageStart, pageLimit)
+	if err != nil {
+		res := &utils.Response{Code: 1000, Msg: err.Error()}
+		res.Json(c)
+		return
+	}
+
+	res := &utils.Response{Code: 200, Msg: "", Data: result}
+	res.Json(c)
+}
+
 func ChatRoomMessagesList(c *gin.Context) {
 	logger := utils.Log()
 	logger.Info("Get Messages List")
